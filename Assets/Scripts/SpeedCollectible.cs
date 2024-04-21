96bd93bfcb0d76d856b93ef97a95a13251884256
ralfomanzur <ralfolyc@gmail.com>
2024-04-20 22:54:35 -0400

    Wrapping Up
    Just some comments on my speed collectible script for the Professor.

--- Assets/Scripts/SpeedCollectible.cs
+++ Assets/Scripts/SpeedCollectible.cs
@@ -11,10 +11,13 @@ public class SpeedCollectible : MonoBehaviour
     // Code that speeds Ruby up, plays a sound, and destroys the pickup object
     void OnTriggerEnter2D(Collider2D other)
     {
+        // Declaring a variable used to get information from RubyController script
         RubyController controller = other.GetComponent<RubyController>();
 
+        // If Ruby is active, do this
         if (controller != null)
         {
+            // Increases speed from 3 to 4.5, destroys speed pick up object, and plays boost sound effect
             controller.speed = 4.5f;
             Destroy(gameObject);
             controller.PlaySound(speedUp);
